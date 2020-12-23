749ed850e4ae1cdf164264f8d4fcc18ca19835b9
Erick Canali <erick.canali@gmail.com>
2020-12-23 09:12:05 -0300

    accepted files extensions upload logic added

--- application.py
+++ application.py
@@ -1,9 +1,8 @@
-import os
-from flask import Flask, flash, redirect, render_template, request, url_for
-from werkzeug.utils import secure_filename
+import os, sqlite3
+from flask import Flask, g, redirect, render_template, request
 
-UPLOAD_FOLDER = '/Users/ecanali/Documents/cs50/final-project/joy-web-app/static/gallery'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+UPLOAD_FOLDER = './static/gallery'
+DATABASE = './database.db'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -12,27 +11,12 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
     return render_template('index.html')
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
-
 @app.route('/gallery', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return render_template('gallery.html')
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return render_template('gallery.html')
     else:
         return render_template('gallery.html')
 
